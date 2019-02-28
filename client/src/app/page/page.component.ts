9f21893866d3107fd102299b80363d96ac070cef
shifra <shif602@gmail.com>
2019-02-28 21:40:17 +0200

    send data to contact service

--- client/src/app/page/page.component.ts
+++ client/src/app/page/page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService} from '../services/contact.service';
+import { Router } from '@angular/router';
+import { Contact } from '../models/contact';
 
 @Component({
   selector: 'app-page',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactService: ContactService, private router: Router) { }
 
   ngOnInit() {
   }
 
+addContact(name,phone,email){
+
+  const contact = <Contact>{
+    name:name.value,
+    email:email.value,
+    phone:phone.value
+  }
+
+  return this.contactService.post(contact).subscribe(res => {
+    console.log(res);
+});
+
+}
 }
