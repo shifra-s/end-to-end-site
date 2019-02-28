d672ac233214c04a7a16914093f865f076438da4
shifra <shif602@gmail.com>
2019-02-28 19:37:13 +0200

    added routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'Page', component: PageComponent},
+  {path:'About', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
