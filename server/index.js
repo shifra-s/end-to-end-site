06f61587ef9e3bb4e793125f85fa5e7c67ea3f45
shifra <shif602@gmail.com>
2019-02-28 23:02:02 +0200

    fixed store contact query

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const PORT = 8888;
 const app = express();
 var mysql = require('mysql');
-app.use(express.json);
+app.use(express.json());
 
 
 var connection = mysql.createConnection({
@@ -20,7 +20,23 @@ app.use(cors());
 app.post('/contact', function (req, res) {
     connection.connect(function (err) {
         let post = req.body;
-        let sql ="INSERT INTO students(`name`,`email`,`phone`) VALUES?"
+        console.log(post)
+        let sql ="INSERT INTO contact SET ?"
+        if (err) throw err;
+        connection.query(sql, post, function (err, result, fields) {
+            if (err) throw err;
+            res.send(result);
+            //end connection
+            connection.end();
+        })
+    })
+});
+
+app.get('/', function (req, res) {
+    connection.connect(function (err) {
+        let post = req.body;
+        let sql ="SELECT * FROM contact(`name`,`email`,`phone`) VALUES?"
+        console.log(sql);
         if (err) throw err;
         connection.query(sql, post, function (err, result, fields) {
             if (err) throw err;
