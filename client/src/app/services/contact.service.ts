ab64543ada0b915feb968b120e006a27af4a9fd6
shifra <shif602@gmail.com>
2019-02-28 21:51:25 +0200

    send data to server

--- client/src/app/services/contact.service.ts
+++ client/src/app/services/contact.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Contact } from '../models/contact';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private HttpClient: HttpClient) { }
+
+
+  post(Contact: Contact): Observable<object> {
+    return this.HttpClient.post('http://localhost:8080/contact', Contact);
+
+  }
+
 }
