60139a3a0efe57bb11da68392e32dd7798bedcd0
shifra <shif602@gmail.com>
2019-03-05 13:11:24 +0200

    updated routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path:'Page', component: PageComponent},
   {path:'About', component: AboutComponent},
+  {path:'Recipes', component: RecipeListComponent},
 ];
 
 @NgModule({
