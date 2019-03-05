cd9d208c9781835bc6b6e542b0d16be53d15e24f
shifra <shif602@gmail.com>
2019-03-05 13:11:58 +0200

    imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,13 +9,20 @@ import { AboutComponent } from './about/about.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { ContactService } from './services/contact.service';
+import { RecipeService } from './services/recipe.service';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeAddComponent } from './recipe-add/recipe-add.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RecipeItemComponent,
+    RecipeAddComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +30,7 @@ import { ContactService } from './services/contact.service';
     FormsModule,
     HttpClientModule
   ],
-  providers: [ContactService],
+  providers: [ContactService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
